5876a54a617e937c614b3dc0f6830f9eadca4cda
sucoso <65839195+nitsuboy@users.noreply.github.com>
2023-07-25 03:48:58 -0300

    gitignore

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddAuthorizationBuilder()
   .AddPolicy("admin", policy =>
         policy
             .RequireRole("admin")
-            .RequireClaim("scope", "modification"));
+            .RequireClaim("scope", "APP"));
 builder.Services.AddHealthChecks();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -19,7 +19,7 @@ builder.Services.AddMySql<SltbetContext>(connectionString[0],Microsoft.EntityFra
 
 builder.Services.AddSwaggerGen(c =>
 {
-     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Description = "API for the Sltbetfk project", Version = "v1" });
+     c.SwaggerDoc("v1", new OpenApiInfo { Title = "SltbetFK", Description = "API for the Sltbetfk project"});
 });
 
 var app = builder.Build();
@@ -33,10 +33,11 @@ app.UseSwaggerUI(c =>
    c.SwaggerEndpoint("/swagger/v1/swagger.json", "API");
 });
 
-app.MapGet("/", () => "i am working :)").WithTags("Teste");
-app.MapGet("/auth", () => "i am working :)").WithTags("Teste").RequireAuthorization("admin");
+app.MapGet("/", () => "up and working").WithTags("Debug");
+app.MapGet("/auth", () => "Authorized").WithTags("Debug").RequireAuthorization("admin");
 
-app.MapGet("/Lutadores", async (SltbetContext db) => await db.Lutadores.ToListAsync()).WithTags("Lutadores");
+app.MapGet("/Lutadores", async (SltbetContext db) => await db.Lutadores.ToListAsync()).WithTags("Lutadores")
+                                                                                      .Produces<IEnumerable<Lutador>>(200);
 app.MapGet("/Lutadores/{nome}", async (string nome,SltbetContext db) => {
                                                                            var l = await db.Lutadores.FindAsync(nome);
                                                                            if (l == null) return Results.NoContent();
@@ -44,6 +45,7 @@ app.MapGet("/Lutadores/{nome}", async (string nome,SltbetContext db) => {
                                                                         }).WithTags("Lutadores")
                                                                           .Produces<Lutador>(200).Produces(204);
 app.MapPost("/Lutadores", async (Lutador lutador, SltbetContext db) => {
+
                                                                         if(db.Lutadores.FirstOrDefault(l => l.Nome == lutador.Nome) != null){
                                                                            return Results.BadRequest("Lutador já existe");
                                                                         }
@@ -76,7 +78,7 @@ app.MapPut("/Lutadores", async (LutadorPost lutador, SltbetContext db) => {
                                                                      Lutador => lutador.Pasta));
                                                                      return Results.Ok("Lutador atualizado");
                                                                   }).WithTags("Lutadores")
-                                                                    .Produces(201).Produces(400).Produces(401)
+                                                                    .Produces(200).Produces(400).Produces(401)
                                                                     .RequireAuthorization("admin");
 app.MapPut("/Lutadores/sumpontuacao", async (LutadorPont lutador, SltbetContext db) => {
                                                                      await db.Lutadores
@@ -92,11 +94,15 @@ app.MapPut("/Lutadores/sumpontuacao", async (LutadorPont lutador, SltbetContext 
                                                                      Lutador => Lutador.Pontos,
                                                                      Lutador => Lutador.Pontos + lutador.Pontos));
                                                                      return Results.Ok("Lutador atualizado");
-                                                                  }).WithTags("Lutadores").RequireAuthorization("admin");
+                                                                  }).WithTags("Lutadores")
+                                                                    .Produces(200).Produces(400).Produces(401)
+                                                                    .RequireAuthorization("admin");
 app.MapDelete("/Lutadores", async (string nome, SltbetContext db) => {
                                                                     await db.Lutadores.Where(Lutador => Lutador.Nome == nome).ExecuteDeleteAsync();
                                                                     return Results.Ok("Lutador deletado");
-                                                                 }).WithTags("Lutadores").RequireAuthorization("admin");
+                                                                 }).WithTags("Lutadores")
+                                                                   .Produces(200).Produces(400).Produces(401)
+                                                                   .RequireAuthorization("admin");
 app.MapPatch("/Lutadores/updatetier", async(SltbetContext db) => {
                                                                      var high = await db.Lutadores.MaxAsync(Lutador => Lutador.Pontos) / 9;
                                                                      await db.Lutadores.ExecuteUpdateAsync(s => 
@@ -110,22 +116,37 @@ app.MapPatch("/Lutadores/updatetier", async(SltbetContext db) => {
                                                                         Lutador.Pontos > high * 2 ? "C"    :
                                                                         Lutador.Pontos > high ? "D" : "F"));
                                                                      return Results.Ok("tiers atualizados");
-                                                                  }).WithTags("Lutadores").RequireAuthorization("admin");
+                                                                  }).WithTags("Lutadores")
+                                                                    .Produces(200).Produces(401)
+                                                                    .RequireAuthorization("admin");
 
-app.MapGet("/Lutas", async (SltbetContext db) => await db.Lutas.ToListAsync()).WithTags("Lutas");
-app.MapGet("/Lutas/{id}", async (int id, SltbetContext db) => await db.Lutas.Where(Luta => Luta.LutaId == id).ToListAsync()).WithTags("Lutas");
+app.MapGet("/Lutas", async (SltbetContext db) => await db.Lutas.ToListAsync()).WithTags("Lutas")
+                                                                              .Produces<IEnumerable<Luta>>(200);
+app.MapGet("/Lutas/{id}", async (int id, SltbetContext db) => {
+                                                               var l = await db.Lutas.Where(Luta => Luta.LutaId == id).ToListAsync();
+                                                               if(!l.Any()) return Results.NoContent();
+                                                               return Results.Ok(l);
+                                                               }).WithTags("Lutas")
+                                                                 .Produces<Luta>(200).Produces(204);
 app.MapGet("/Lutas/data/{data}", async (string data, SltbetContext db) => {
                                                                               await db.Lutas.Where(Luta => Luta.Data == DateTime.Parse(data)).ToListAsync();
                                                                            }).WithTags("Lutas");
 app.MapPost("/Lutas", async (Luta luta, SltbetContext db) => {
+                                                               if(db.Lutas.FirstOrDefault(l => l.LutaId == luta.LutaId) != null){
+                                                                  return Results.BadRequest("Luta já existe");
+                                                               }
                                                                await db.Lutas.AddAsync(luta);
                                                                await db.SaveChangesAsync();
-                                                               return Results.Ok("Luta inserida");
-                                                             }).WithTags("Lutas").RequireAuthorization("admin");
+                                                               return Results.CreatedAtRoute("PostLuta", new {lutaid = luta.LutaId}, luta);
+                                                             }).WithName("PostLuta").WithTags("Lutas")
+                                                               .Produces<Luta>(201).Produces(400).Produces(401)
+                                                               .RequireAuthorization("admin");
 app.MapDelete("/Lutas", async (int id, SltbetContext db) => {
                                                                await db.Lutas.Where(Luta => Luta.LutaId == id).ExecuteDeleteAsync();
                                                                return Results.Ok("Luta deletada");
-                                                            }).WithTags("Lutas").RequireAuthorization("admin");
+                                                            }).WithTags("Lutas")
+                                                              .Produces(200).Produces(400).Produces(401)
+                                                              .RequireAuthorization("admin");
 
 app.Run();
 
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Sltbetfk
- 
+![](https://img.shields.io/badge/MySQL-%231e4c68?style=for-the-badge&logo=MySQL&logoColor=%23e3752c)
+![](https://img.shields.io/badge/C%23-%2366217B?style=for-the-badge&logo=csharp&logoColor=white)
+![](https://img.shields.io/badge/PYTHON-%233776AB?style=for-the-badge&logo=python&logoColor=white)
+
+
+Generate aplication bearer token
+
+    dotnet user-jwts create --scope "APP" --role "admin"
